2c12a2257fd284581898a8593b203655888b47bb
ergunDevv <ergncyln2127088@gmail.com>
2022-12-14 00:24:19 +0300

    Final Project

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsLinkedin,BsGithub } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+    <a href="https://github.com/ergunDevv"target="_blank" rel="noopener noreferrer"><BsGithub /> </a>
     </div>
     <div>
-      <FaFacebookF />
+     <a href="https://www.linkedin.com/in/ergün-ceylan-9664aa229/"target="_blank" rel="noopener noreferrer"><BsLinkedin /></a> 
     </div>
     <div>
-      <BsInstagram />
+    <a href="https://twitter.com/Ergun_Dev"target="_blank" rel="noopener noreferrer"><BsTwitter /></a>
     </div>
   </div>
 );
--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+      {[images.html, images.react, images.css].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
