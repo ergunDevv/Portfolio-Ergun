b04240e983982fda66cc5d3cf1d2a5cc742a0d11
ergunDevv <ergncyln2127088@gmail.com>
2022-12-13 18:40:54 +0300

    Final version without projects

--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './Header.scss'
-import {motion} from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 
-import {images} from '../../constants'
-
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
+import './Header.scss';
 
 const scaleVariants = {
   whileInView: {
@@ -33,26 +33,26 @@ const Header = () => (
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Full Stack Web Developer 💻</p>
-          
+          <p className="p-text">Full-Stack Developer</p>
+          {/* <p className="p-text"></p> */}
         </div>
       </div>
     </motion.div>
 
-    {/* <motion.div
+    <motion.div
       whileInView={{ opacity: [0, 1] }}
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" className='profile__image'/>
-      <motion.img
+      {/* <img src={images.profile} alt="profile_bg" /> */}
+      {/* <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
         src={images.circle}
         alt="profile_circle"
         className="overlay_circle"
-      />
-    </motion.div> */}
+      /> */}
+    </motion.div>
 
     <motion.div
       variants={scaleVariants}
@@ -67,4 +67,5 @@ const Header = () => (
     </motion.div>
   </div>
 );
-export default Header
+
+export default AppWrap(Header, 'home');
